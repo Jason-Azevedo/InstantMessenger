98710cc5d40c3480b6d5625eff50e3fa0719da6f
Jason Azevedo <jasonazevedo03@gmail.com>
2020-11-24 18:21:15 +0200

    Completed UI for Login and Signup screen

--- client/src/Pages/Login.tsx
+++ client/src/Pages/Login.tsx
@@ -5,17 +5,18 @@ export default function Login() {
   return (
     <div className="container--middle">
       <form className="form">
-        <h1 className="form__header">Please Login</h1>
+        <h2 className="form__subheader">Welcome to I.M.</h2>
+        <h1 className="form__header">Login</h1>
 
         <label htmlFor="" className="form__label">
-          Username
+          Username / Email
         </label>
-        <input type="text" className="form__input" />
+        <input type="text" className="form__input" required />
 
         <label htmlFor="" className="form__label">
           Password
         </label>
-        <input type="password" className="form__input" />
+        <input type="password" className="form__input" required />
 
         <p className="form__text">
           Don't have an account? <Link to="/signup">Create one!</Link>
--- client/src/Pages/Signup.tsx
+++ client/src/Pages/Signup.tsx
@@ -1,9 +1,36 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Signup() {
   return (
-    <>
-      <h1>This is the signup page!</h1>
-    </>
+    <div className="container--middle">
+      <form className="form">
+        <h2 className="form__subheader">Welcome to I.M.</h2>
+        <h1 className="form__header">Sign Up</h1>
+
+        <label htmlFor="" className="form__label">
+          Username
+        </label>
+        <input type="text" className="form__input" required />
+
+        <label htmlFor="" className="form__label">
+          Email
+        </label>
+        <input type="email" className="form__input" required />
+
+        <label htmlFor="" className="form__label">
+          Password
+        </label>
+        <input type="password" className="form__input" required />
+
+        <p className="form__text">
+          Already have an account? <Link to="/login">Login!</Link>
+        </p>
+
+        <button type="submit" className="btn">
+          CREATE
+        </button>
+      </form>
+    </div>
   );
 }
